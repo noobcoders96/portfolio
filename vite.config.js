12a617f389e7eb3bc551630ec70e7c9138ae70d6
karthik <ishowcomplex16@gmail.com>
2026-08-05 08:49:03 +0530

    url changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // If you deploy to a user/org site (username.github.io repo), leave base as '/'.
 export default defineConfig({
   plugins: [react()],
-  base: 'https://karthikeyan-srinivasan.github.io/portfolio',
+  base: 'https://noobcoders96.github.io/portfolio',
 })
